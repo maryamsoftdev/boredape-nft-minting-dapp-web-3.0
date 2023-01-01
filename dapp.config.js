510c33555e13d962bc985cd624755d466a462a5e
maryamsoftdev <nabeelahmaryaam@gmail.com>
2023-01-01 21:27:04 +0500

    intializing my first Next js and web 3.0 project

---
+++ dapp.config.js
@@ -0,0 +1,10 @@
+const config = {
+  title: 'BoredApe Dapp',
+  description: 'A showcase dapp which is built for you to create your own',
+//   contractAddress: '0x0312e42e4b55823a3C41769DC0B07F382dECc247',
+//   maxMintAmount: 10,
+//   presaleMaxMintAmount: 3,
+//   price: 0.01
+}
+
+export { config }
