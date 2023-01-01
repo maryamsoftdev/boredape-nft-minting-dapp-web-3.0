947a06abf71877d4f8dabbfd84c9bb3ce66daa4b
maryamsoftdev <nabeelahmaryaam@gmail.com>
2023-01-02 00:26:01 +0500

    added wallet options through block native on borad

--- dapp.config.js
+++ dapp.config.js
@@ -6,5 +6,57 @@ const config = {
 //   presaleMaxMintAmount: 3,
 //   price: 0.01
 }
+const onboardOptions = {
+   dappId: 'cea1a743-6361-4e26-9ac9-d963253b9d96',
+   networkId: 4, //rinkeby
+   walletSelect:{
+   wallets:[
+     { walletName: "metamask", preferred: true },
+     { walletName: "coinbase", preferred: true },
+     { walletName: "walletLink", preferred: true, rpcUrl: RPC_URL, appName:'BoredApe Dapp', },
+     { walletName: "trust", preferred: true, rpcUrl: RPC_URL },
+     { walletName: "authereum" },
+     {
+    walletName: 'trezor',
+    appUrl: APP_URL,
+    email: CONTACT_EMAIL,
+    rpcUrl: RPC_URL
+  },
+  {
+    walletName: 'ledger',
+    rpcUrl: RPC_URL
+  },
+  {
+    walletName: 'lattice',
+    rpcUrl: RPC_URL,
+    appName: APP_NAME
+  },
+  {
+    walletName: 'keepkey',
+    rpcUrl: RPC_URL
+  },
+  {
+    walletName: 'mewwallet',
+    rpcUrl: RPC_URL
+  },
+  { 
+    walletName: 'cobovault',
+    rpcUrl: RPC_URL,
+    appName: APP_NAME,
+  },
+  { 
+    walletName: 'keystone',
+    rpcUrl: RPC_URL,
+    appName: APP_NAME,
+  },
+  {
+    walletName: "fortmatic",
+    apiKey: FORTMATIC_KEY,
+    preferred: true
+  },
+
+
+]
+}
 
 export { config }
